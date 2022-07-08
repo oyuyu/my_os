d4cad491594050ee71f476c8fb964bc2cf5836ac
whr <huiru.wang02@weimob.com>
2022-07-08 16:34:39 +0800

    路由配置

--- config/routes.ts
+++ config/routes.ts
@@ -2,30 +2,35 @@ export default [
   {
     //是否严格模式   location与path是否完全匹配
     exact: true,
-    path: '/',
-    title: '说的人',
-    component: '@/layouts/index',
-    //子路由
-    routes: [
-      {
-        path: '/useRef',
-        title: 'useRef',
-        component: '@/pages/index',
-      },
-    ],
-  },
-  {
     path: '/login',
     title: '用户登录',
     component: '@/pages/index',
   },
   {
-    path: '/user',
-    component: '@/pages/index',
-    //重定向
-    redirect: '/useRef',
-
+    path: '/',
+    component: '@/layouts/index',
     // 路由的高阶组件封装    常用于路由级别的权限校验
     wrappers: ['@/wrapper/auth'],
+    routes: [
+      {
+        path: '/',
+        redirect: '/home', // 配置路由跳转
+      },
+      {
+        path: '/params',
+        component: '@/pages/routers/params',
+      },
+      {
+        path: '/hooks',
+        //子路由
+        routes: [
+          {
+            path: '/hooks/useRef',
+            title: 'useRef',
+            component: '@/pages/index',
+          },
+        ],
+      },
+    ],
   },
 ];
--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,3 +1,3 @@
 export default () => {
-  return { isLogin: false };
+  return { isLogin: true };
 };
