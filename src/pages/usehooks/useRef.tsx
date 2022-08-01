eabde124379bf4b5ccfc9650b1c15eb711e39c0b
whr <huiru.wang02@weimob.com>
2022-08-01 18:59:23 +0800

    useRef

--- src/pages/usehooks/useRef.tsx
+++ src/pages/usehooks/useRef.tsx
@@ -6,22 +6,30 @@ import React, {
 } from 'react';
 import { Button } from 'antd';
 
-const Son = (props, ref) => {
+const Son = forwardRef((props, ref) => {
+  const [message, setMessage] = useState(1);
+
   useImperativeHandle(ref, () => {
     return { handleChange };
   });
-  const handleChange = () => {};
-  return <Button onClick={handleChange}>更改父级State</Button>;
-};
+  const handleChange = () => {
+    setMessage(message + 1);
+  };
+  return <div>{message}</div>;
+});
 
 const Father = () => {
-  const sonref = useRef(null);
-  const [message, setMessage] = useState('');
+  const sonref = useRef<any>(null);
 
   return (
     <div>
-      <div> 我是父级的:</div>
+      <div> 我是父级的</div>
+      <Button onClick={() => sonref.current?.handleChange()}>
+        更改父级State
+      </Button>
       <Son ref={sonref} />
     </div>
   );
 };
+
+export default Father;
