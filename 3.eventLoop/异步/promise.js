3fe74f510f4d9cba54c23560bc619fbc5d3e5bb3
whr <huiru.wang02@weimob.com>
2022-08-17 19:05:29 +0800

    what: 简短的描述干了什么
    why:
    
    * 我为什么要这么做
    
    how:
    
    *我是怎么做的？这么做有什么副作用？

--- 3.eventLoop/异步/promise.js
+++ 3.eventLoop/异步/promise.js
@@ -16,48 +16,16 @@ const p5 = Promise.reject('err5')
  * 2.任意一个Promise失败
  */
 
-// Promise._all = (promises) => {
-//   return new Promise((rs, rj) => {
-//     // 计数器
-//     let count = 0
-//     // 存放结果
-//     let result = []
-//     const len = promises.length
-
-//     if (len === 0) {
-//       return rs([])
-//     }
-
-//     promises.forEach((p, i) => {
-//       // 注意有的数组项有可能不是Promise，需要手动转化一下
-//       Promise.resolve(p)
-//         .then((res) => {
-//           count += 1
-//           // 收集每个Promise的返回值
-//           result[i] = res
-//           // 当所有的Promise都成功了，那么将返回的Promise结果设置为result
-//           if (count === len) {
-//             rs(result)
-//           }
-//           // 监听数组项中的Promise catch只要有一个失败，那么我们自己返回的Promise也会失败
-//         })
-//         .catch(rj)
-//     })
-//   })
-// }
-
 Promise._all = (promises) => {
   return new Promise((resolve, reject) => {
     let res = []
     promises.forEach((p) => {
-      Promise.resolve(p)
-        .then((_res) => {
-          res.push(_res)
-          if (res.length === promises.length) {
-            resolve(res)
-          }
-        })
-        .catch(reject)
+      p.then((_res) => {
+        res.push(_res)
+        if (res.length === promises.length) {
+          resolve(res)
+        }
+      }).catch(reject)
     })
   })
 }
