9eacd8020d1edf49937d415e8dd284adecfb8ff6
whr <huiru.wang02@weimob.com>
2022-09-08 22:01:19 +0800

    ...

--- src/pages/routers/params.tsx
+++ src/pages/routers/params.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
 export default (props: any) => {
   const { match, location, history, route, routes, children } = props;
   console.log(props);
-  return <div>11</div>;
+  return (
+    <div className={styles.outer}>
+      <div className={styles.inner}></div>
+    </div>
+  );
 };
